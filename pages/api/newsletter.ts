688ef9e9abdc4658e920af6b13686e045709c86c
Paras <paras.jain2@publicissapient.com>
2025-10-16 16:22:55 +0530

    code updated

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,14 +1,25 @@
+import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { email } = req.body;
     if (!email || !email.includes("@")) {
       return res.status(422).json({ message: "Invalid email address" });
     }
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://parasmanijain2208:Gulshan1006@cluster0.h1buo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+
+    const db = client.db();
+    await db.collection('emails').insertOne({
+      email
+    });
+    client.close();
     return res.status(201).json({
-        message: "Signed up!"
-    })
+      message: "Signed up!",
+    });
   }
 };
 
