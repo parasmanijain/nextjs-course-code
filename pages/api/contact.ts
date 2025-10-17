0336b47e0793f27f0fff25113725c42530d59dd1
Paras <paras.jain2@publicissapient.com>
2025-10-17 12:56:08 +0530

    code updated

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,15 +1,51 @@
+import { MongoClient, ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+interface Message {
+  email: string;
+  name: string;
+  message: string;
+  id?: ObjectId;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
-    if(!email || !name || !message || !email.includes('@') || name.trim()=== '' || message.trim()==='') {
-       return res.status(422).json({message:"Invalid input."}) 
+    if (
+      !email ||
+      !name ||
+      !message ||
+      !email.includes("@") ||
+      name.trim() === "" ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({ message: "Invalid input." });
     }
-    const newMessage = {
-        email,name,message
+    const newMessage: Message = {
+      email,
+      name,
+      message,
+    };
+    let client: MongoClient;
+    try {
+      client = await MongoClient.connect(process.env.DATABASE_NAME);
+    } catch (error) {
+      return res.status(500).json({ message: "Could not connect to database" });
     }
-    return res.status(201).json({message:"Successfully stored the message"})
+
+    try {
+      const db = client.db("blog");
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+      res
+        .status(201)
+        .json({ message: "Successfully stored message!" + newMessage });
+    } catch (error) {
+      res.status(500).json({ message: "Storing message failed" });
+    }
+
+    client.close();
+    return;
   }
 };
 
