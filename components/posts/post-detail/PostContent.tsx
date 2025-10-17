19e4c7b7ec47d0f8ecf3bdfa77c63dce0cce68ca
Paras <paras.jain2@publicissapient.com>
2025-10-17 11:39:38 +0530

    code updated

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,4 +1,6 @@
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import  atomDark  from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
 import classes from "./PostContent.module.scss";
 import { PostHeader } from "./PostHeader";
 import Image from "next/image";
@@ -12,13 +14,33 @@ export const PostContent = ({ post }) => {
         alt={alt}
         width={600}
         height={300}
+        priority
       />
     ),
+   code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
   };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown components={customComponents}>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
