72ffa9222139d73f51894fb7fa90b6b1932e2f53
Paras <paras.jain2@publicissapient.com>
2025-10-17 11:01:59 +0530

    code updated

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,13 +1,24 @@
 import ReactMarkdown from "react-markdown";
 import classes from "./PostContent.module.scss";
 import { PostHeader } from "./PostHeader";
+import Image from "next/image";
 
 export const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customComponents = {
+    img: ({ src, alt }) => (
+      <Image
+        src={`/images/posts/${post.slug}/${src}`}
+        alt={alt}
+        width={600}
+        height={300}
+      />
+    ),
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>{post.content}</ReactMarkdown>
     </article>
   );
 };
