2c65c3e0c9c78a2df461ea252fc12a9bdb73d9d2
Paras <paras.jain2@publicissapient.com>
2025-10-18 22:22:37 +0530

    code updated

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,27 @@
 import { UserProfile } from "@/components/profile/UserProfile";
+import { getSession } from "next-auth/react";
 
-function ProfilePage() {
-  return <UserProfile />;
+function ProfilePage({session}) {
+  return <UserProfile session={session} />;
+}
+
+export async function getServerSideProps({ req }) {
+ const session = await getSession({ req });
+
+ if(!session) {
+  return {
+    redirect:{
+      destination: '/auth',
+      permanent:false
+    }
+  }
+ }
+
+ return  {
+  props:{
+    session
+  }
+ }
 }
 
 export default ProfilePage;
