300e36df1c37d5a7a22b420bab528316808e8929
Paras <paras.jain2@publicissapient.com>
2025-10-18 17:11:20 +0530

    code updated

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -4,26 +4,44 @@ import { MongoClient } from "mongodb";
 import { hashPassword } from "@/lib/auth";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body;
-  if(!email || !email.includes('@') || !password || password.trim().length<7) {
-    return res.status(422).json({message: 'Invalid input - password should be atleast 7 characters long.'})
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      return res.status(422).json({
+        message:
+          "Invalid input - password should be atleast 7 characters long.",
+      });
+    }
+    let client: MongoClient;
+    try {
+      client = await connectToDatabase();
+    } catch (error) {
+      return res.status(500).json({ message: "" });
+    }
+
+    const db = client.db();
+    const existingUser = await db.collection("users").findOne({
+      email,
+    });
+    if (existingUser?._id) {
+      res.status(422).json({ message: "User already exists!" });
+    } else {
+      const hashedPassword = await hashPassword(password);
+
+      const result = await db.collection("users").insertOne({
+        email,
+        password: hashedPassword,
+      });
+      res.status(201).json({ message: "Created user!" });
+    }
+    client.close();
+    return;
   }
-  let client:MongoClient;
-  try {
-    client = await connectToDatabase();
-  } catch(error) {
-    return res.status(500).json({message:""});
-  }
-
-  const db = client.db();
-  const hashedPassword = await hashPassword(password);
-
-  const result = await db.collection("users").insertOne({
-    email,
-    password:hashedPassword
-  });
-  res.status(201).json({message:'Created user!'});
-
 };
 
 export default handler;
